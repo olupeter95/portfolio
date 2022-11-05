24ad4f0a256cc11aeef317c42b77542bf6d467a8
Oluwasola peter <52799097+olupeter95@users.noreply.github.com>
2022-11-05 01:22:20 +0100

    change default home page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home/page', [IndexController::class, 'home'])->name('home');
+Route::get('/', [IndexController::class, 'home'])->name('home');
