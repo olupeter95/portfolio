d4e94e257d43ac5eb7376e82e34adcfee1a9bdce
Oluwasola peter <52799097+olupeter95@users.noreply.github.com>
2022-11-14 09:16:36 +0100

    view about in frontend

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\About;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
     public function home()
     {
-        return view('frontend.index');
+        $about = About::first();
+        return view('frontend.index', compact('about'));
     }
 }
